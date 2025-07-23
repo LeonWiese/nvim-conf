36ff45b247455d6b20b5ffa444554ffbc9ea0728
Leon Wiese <leonwiese@live.de>
2025-07-23 16:59:52 +0200

    Change theme location to repository

--- init.lua
+++ init.lua
@@ -1119,16 +1119,10 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'local/darcula',
+    'LeonWiese/darcula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     lazy = false,
-    config = function()
-      require('darcula').setup()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'darcula'
-    end,
+    opts = {},
   },
 
   {
