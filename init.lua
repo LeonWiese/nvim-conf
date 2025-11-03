aebb115a20c13c6f663527edb17735c52cc66d70
Leon Wiese <leonwiese@live.de>
2025-11-03 15:27:25 +0100

    Add new search key binding

--- init.lua
+++ init.lua
@@ -577,11 +577,16 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sG', function()
-        builtin.live_grep { cwd = get_current_dir(), prompt_title = 'Live Grep in ' .. vim.fn.expand '%:p:h:t' .. '/' }
+        builtin.live_grep {
+          cwd = get_current_dir(),
+          additional_args = { '--no-ignore' },
+          prompt_title = 'Live Grep in ' .. vim.fn.expand '%:p:h:t' .. '/',
+        }
       end, { desc = '[S]earch by [G]rep in current directory' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>su', telescope.extensions.undo.undo, { desc = '[S]earch [U]ndo tree' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume last picker' })
+      vim.keymap.set('n', '<leader>sR', builtin.pickers, { desc = '[S]earch [R]esume search picker' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', function()
         builtin.buffers { sort_mru = true }
